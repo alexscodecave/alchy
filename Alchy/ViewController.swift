1bc6269324cb7298421456543ba16e7075503d3b
Alex Biddle <alexbiddle@alexs-macbook-air.local>
2018-10-20 19:48:38 +0100

    second commit

--- Alchy/ViewController.swift
+++ Alchy/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("hello")
+        print("first commit")
+        
     }
 
     override func didReceiveMemoryWarning() {

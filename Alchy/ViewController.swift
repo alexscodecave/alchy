095b2540c1ab0aab3ae6d654dccd08197a8afacf
Alex Biddle <alexbiddle@alexs-macbook-air.local>
2018-10-20 19:46:01 +0100

    Initial Commit

---
+++ Alchy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alchy
+//
+//  Created by Alex Biddle on 20/10/2018.
+//  Copyright © 2018 Alex Biddle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
